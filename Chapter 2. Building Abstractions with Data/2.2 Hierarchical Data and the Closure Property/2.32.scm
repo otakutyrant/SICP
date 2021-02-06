cd24e5c7c1f1754d686ddc57c60cad8ab4b3b3bc
acgtyrant <acgtyrant@gmail.com>
2021-02-06 11:35:12 +0800

    2.32.scm: fix bug that is using an empty set instead of a valid subsets containing the former

--- Chapter 2. Building Abstractions with Data/2.2 Hierarchical Data and the Closure Property/2.32.scm
+++ Chapter 2. Building Abstractions with Data/2.2 Hierarchical Data and the Closure Property/2.32.scm
@@ -2,11 +2,11 @@
   (subsets s)
   (if
     (null? s)
-    s
+    (list s)
     (let
       ((rest (subsets (cdr s))))
       (append rest (map (lambda (set) (cons (car s) set)) rest))
     )
   )
 )
-(subsets (list 1 2))
+(subsets (list 1))
